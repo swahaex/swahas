de8a99fb58188e8d8f7e94ac64e9a09bd2b5581d
Swaha Nayak <swahanayak1@gmail.com>
2026-02-17 09:17:26 +0530

    Create OOPSBannerUC4 to print a banner
    Add a class to display a banner in the console.

---
+++ OOPSBannerUC4.java
@@ -0,0 +1,21 @@
+public class OOPSBannerUC4 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+
+            " *****   *****   *****   ***** ",
+            "*     * *     * *     * *     ",
+            "*     * *     * *     * *     ",
+            "*     * *     * *****   ***** ",
+            "*     * *     * *           *",
+            "*     * *     * *           *",
+            " *****   *****   *       *****"
+
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
